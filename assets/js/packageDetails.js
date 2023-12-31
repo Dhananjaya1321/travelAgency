093408308417fb319908d1dc923620925eadd09b
Dhananjaya1321 <blackisurudananjaya@gmail.com>
2023-12-31 21:37:59 +0530

    Created setPackageDetails function

--- assets/js/packageDetails.js
+++ assets/js/packageDetails.js
@@ -1,12 +1,12 @@
 let packageDetails = [
     {
-        "packageId":"PKG001",
-        "images": {
-            "image-1": "",
-            "image-2": "",
-            "image-3": "",
-            "image-4": "",
-            "image-5": "",
+        "packageId": "PKG001",
+        "locationPhotos": {
+            "image1": "assets/images/1677609309357.jpg",
+            "image2": "assets/images/1677609309357.jpg",
+            "image3": "assets/images/1677609309357.jpg",
+            "image4": "assets/images/1677609309357.jpg",
+            "image5": "assets/images/1677609309357.jpg",
         },
         "packageTopic": "08 Night / 09 Days Culture Adventure",
         "itinerary": [
@@ -41,26 +41,70 @@ let packageDetails = [
                 "description": "kddkccccccccccccccccccccccccdk"
             },
         ],
-        "tourDetails":{
-            "PackageType":"Culture Adventure",
-            "PackageDuration":"8 Nights / 9 Days",
-            "Locations":"Dambulla Golden Rock Temple - Mirissa Beach - Sigiriya Rock Fortress - Temple Of The Tooth Relic - Yala National Park - Half Day Jeep Safari",
-            "peopleCount":"2 ADULT",
+        "tourDetails": {
+            "PackageType": "Culture Adventure",
+            "PackageDuration": "8 Nights / 9 Days",
+            "Locations": "Dambulla Golden Rock Temple - Mirissa Beach - Sigiriya Rock Fortress - Temple Of The Tooth Relic - Yala National Park - Half Day Jeep Safari",
+            "peopleCount": "2 ADULT",
         },
-        "price":1200
+        "price": "1200 USD"
     }
 ]
 
 $(".details-btn").click(function () {
-   let packageId=$(this).attr("value");
-   $("#home-section,#offers-section,#why-choose-section,#transport-section,#feedback-section").css("display","none");
-   $("#package-details-page").css("display","flex");
+    let packageId = $(this).attr("value");
+    setPackageDetails(searchPackageDetails(packageId));
+    $("#home-section,#offers-section,#why-choose-section,#transport-section,#feedback-section").css("display", "none");
+    $("#package-details-page").css("display", "flex");
 });
 
 function searchPackageDetails(packageId) {
-    for (let i = 0; i < packageDetails.length; i++) {
-        if (packageDetails[i].packageId===packageId){
+    for (let i = 0; i <= packageDetails.length; i++) {
+        if (packageDetails[i].packageId === packageId) {
             return i;
         }
     }
+}
+
+function setPackageDetails(i) {
+    $("#location-img-1").css({
+        "background": `url(${packageDetails[i].locationPhotos.image1})`,
+        "backgroundPosition": "center",
+        "backgroundSize": "cover"
+    });
+    $("#location-img-2").css({
+        "background": `url("${packageDetails[i].locationPhotos.image2}")`,
+        "backgroundPosition": "center",
+        "backgroundSize": "cover"
+    });
+    $("#location-img-3").css({
+        "background": `url("${packageDetails[i].locationPhotos.image3}")`,
+        "backgroundPosition": "center",
+        "backgroundSize": "cover"
+    });
+    $("#location-img-4").css({
+        "background": `url("${packageDetails[i].locationPhotos.image4}")`,
+        "backgroundPosition": "center",
+        "backgroundSize": "cover"
+    });
+    $("#location-img-5").css({
+        "background": `url("${packageDetails[i].locationPhotos.image5}")`,
+        "backgroundPosition": "center",
+        "backgroundSize": "cover"
+    });
+    $("#package-topic").text(packageDetails[i].packageTopic);
+    for (let itinerary in packageDetails[i].itinerary) {
+        $("#itinerary>div").append(
+            `<div class="itinerary-detail">
+                <p>${packageDetails[i].itinerary[itinerary].day} :</p>
+                <p>Day ${packageDetails[i].itinerary[itinerary].fromTo}</p>
+                <p>${packageDetails[i].itinerary[itinerary].description}</p>
+            </div>`
+        );
+    }
+    $("#package-page-package-type>p").text(packageDetails[i].tourDetails.PackageType);
+    $("#package-page-package-duration>p").text(packageDetails[i].tourDetails.PackageDuration);
+    $("#package-page-package-location>p").text(packageDetails[i].tourDetails.peopleCount);
+    $("#package-page-package-people>p").text(packageDetails[i].tourDetails.peopleCount);
+    $("#price>p").text(packageDetails[i].price);
 }
